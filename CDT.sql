73ee1a688a37d0c665a59ecea2684b07c31a3e6c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2019-09-08 22:09:33 -0400

    Add files via upload
    Showcases create, drop, and truncate.

---
+++ CDT.sql
@@ -0,0 +1,35 @@
+create database zack;
+
+use zack;
+
+create table testTable(
+
+col1 int(1),
+col2 varchar(3)
+
+);
+
+insert testTable (col1, col2)
+values (1, 3);
+
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+insert testTable (col1, col2)
+values (1, 3);
+
+select * from testTable;
+
+truncate table testTable;
+
